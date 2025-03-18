e43d6d28f6954bfde4ffc5f0090df7cfe396b954
Kakaroto2003 <caio.alexandre8000@gmail.com>
2025-03-17 22:16:15 -0300

    add

---
+++ PRATICA/AULA_5 (Java Introdução)/script.js
@@ -0,0 +1,16 @@
+//window.alert("Olá");
+//console.log("Hello World!!!");
+
+//let nome = prompt("Qual o seu nome?");
+//console.log(nome);
+
+let i=0
+while(i<10){
+    console.log(i)
+    i++
+    //i++ = i+1;
+}
+
+//for (let i=0; i<10; i++){
+//    console.log(i)
+//}
