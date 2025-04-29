2835dace41a946d413856042a294adc92bc5965e
CaioAlexandre <caio.alexandre8000@gmail.com>
2025-04-28 22:20:17 -0300

    add

--- PRATICA/AULA/SERVIDOR/Servidor.js
+++ PRATICA/AULA/SERVIDOR/Servidor.js
@@ -1,4 +1,6 @@
 require("colors");
+let bodyParser = require("body-parser")
+
 
 // inclui o módulo http
 var http = require('http');
@@ -9,6 +11,8 @@ var express = require('express');
 // os métodos / funções existentes no framework
 // express
 var app = express () ;
+app.use(bodyParser.urlencoded({extended: false }))
+app.use(bodyParser.json())
 
 // método use() utilizado para definir em qual
 // pasta estará o conteúdo estático
@@ -24,3 +28,43 @@ server.listen(80);
 console. log ("servidor rodando...");
 
 console.log("Olá Mundo!".rainbow);
+
+
+// Exemplos de GET e POST
+
+app.get("/inicio", function(requisição, reposta){
+    reposta.redirect('Aula_1 HTML/index.html')
+})
+
+app.post('/inicio', function(requisição, resposta){
+    resposta.redirect('Aula_1 HTML/index.html')
+})
+
+app.get('/cadastrar', function(requisição, resposta){
+    let nome = requisição.query.Nome;
+    let email = requisição.query.Email;
+    let senha = requisição.query.Senha;
+    let nascimento = requisição.query.Nascimento;
+
+    console.log(nome, email, senha, nascimento);
+
+    resposta.render('Resposta.ejs',
+         {mensagem: "Usuário cadastrado com sucesso!", usuário: nome, login: email})
+})
+
+app.post('/cadastrar', function(requisição, resposta){
+    let nome = requisição.body.Nome;
+    let email = requisição.body.Email;
+    let senha = requisição.body.Senha;
+    let nascimento = requisição.body.Nascimento;
+
+    console.log(nome, email, senha, nascimento);
+
+    resposta.render('Resposta.ejs',
+        {mensagem: "Usuário cadastrado com sucesso!", usuário: nome, login: email})
+})
+
+app.get('/for_ejs', function(requisição, resposta){
+    let num = requisição.query.num;
+    resposta.render('Exemplo_FOR.ejs', {tamanho: num});
+})
