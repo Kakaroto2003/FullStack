974e598ba63429963c8e18967af631f0c6e66648
CaioAlexandre <caio.alexandre8000@gmail.com>
2025-05-12 22:27:54 -0300

    add

--- PRATICA/SERVIDOR/Servidor.js
+++ PRATICA/SERVIDOR/Servidor.js
@@ -1,95 +1,135 @@
 require("colors");
-let bodyParser = require("body-parser")
-var http = require('http'); // inclui o módulo http
-var express = require('express'); // inclui o módulo express
-
-var mongodb = require("mongodb");
+const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
-const uri = 'mongodb+srv://caioalexandre8000:<wYt6cG9YH6wLtrHB>@caioalexandre.qtavtvo.mongodb.net/?retryWrites=true&w=majority&appName=CaioAlexandre'
-const client = new MongoClient(uri, { useNewUrlParser: true });
 
-var dbo = client.db("exemplo_bd");
-var usuarios = dbo.collection("usuarios");
+const uri = 'mongodb+srv://caioalexandre8000:Caio14082003@caioalexandre.qtavtvo.mongodb.net/?retryWrites=true&w=majority&appName=CaioAlexandre';
 
+const app = express();
 
-// cria a variável app, pela qual acessaremos os métodos / funções existentes no framework express
-var app = express () ;
-app.use(bodyParser.urlencoded({extended: false }))
-app.use(bodyParser.json())
+// Middlewares
+app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
 
-// método use() utilizado para definir em qual pasta estará o conteúdo estático
-app. use(express.static('./public'));
+// Conectar ao banco antes de iniciar o servidor
+MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(client => {
+    const db = client.db("exemplo_bd");
+    const usuarios = db.collection("usuarios");
 
-
-var server = http.createServer(app); // cria o servidor
-server.listen(80); // define o número da porta que o servidor ouvirá
-
-// mensagem exibida no console para debug
-console. log ("servidor rodando...");
-console.log("Olá Mundo!".rainbow);
-
-
-// Exemplos de GET e POST
-app.get("/inicio", function(requisição, reposta){
-    reposta.redirect('Aula_1 HTML/index.html')
-})
-
-app.post('/inicio', function(requisição, resposta){
-    resposta.redirect('Aula_1 HTML/index.html')
-})
-
-app.get('/cadastrar', function(requisição, resposta){
-    let nome = requisição.query.Nome;
-    let email = requisição.query.Email;
-    let senha = requisição.query.Senha;
-    let nascimento = requisição.query.Nascimento;
-    console.log(nome, email, senha, nascimento);
-
-    let data = {db_nome: nome, db_email: email, db_senha: senha, db_nascimento: nascimento}
-    usuarios.insertOne(data, function(err){
-        if (err){
-            resposta.render('Resposta.ejs',
-                {mensagem: "Erro ao cadastrar usuário!", usuario: nome, login: email})
-        }else{
-            resposta.render('Resposta.ejs',
-                {mensagem: "Usuário cadastrado com sucesso!", usuario: nome, login: email})
-        }
-    })
-
-})
-
-/**app.post('/cadastrar', function(requisição, resposta){
-    let nome = requisição.body.Nome;
-    let email = requisição.body.Email;
-    let senha = requisição.body.Senha;
-    let nascimento = requisição.body.Nascimento;
-    console.log(nome, email, senha, nascimento);
-
-    resposta.render('Resposta.ejs',
-        {mensagem: "Usuário cadastrado com sucesso!", usuario: nome, login: email}) 
-}) */
-
-app.get('/for_ejs', function(requisição, resposta){
-    let num = requisição.query.num;
-    resposta.render('Exemplo_FOR.ejs', {tamanho: num});
-})
-
-app.post('/login', function(requisição, resposta){
-    let email = requisição.body.email;
-    let senha = requisição.body.senha;
-
-    console.log(email, senha);
-
-    let data = {db_email: email, db_senha: senha};
-    usuarios.find(data).toArray(function(err, items) {
-        console.log(items);
-        if (items.length == 0) {
-          resp.render('resposta_usuario', {resposta: "Usuário/senha não encontrado!"})
-        }else if (err) {
-          resp.render('resposta_usuario', {resposta: "Erro ao logar usuário!"})
-        }else {
-          resp.render('resposta_usuario', {resposta: "Usuário logado com sucesso!"})        
-        };
-      });
-  
+    // Rota inicial
+    app.get('/', (req, res) => {
+      res.redirect('LAB/Project.html');
     });
+
+    app.get('/cadastra', (req, res) => {
+      res.redirect('LAB/LAB_8 - GET_POST_TEMPLATE/cadastro.html');
+    });
+
+    app.get('/login', (req, res) => {
+      res.redirect('LAB/LAB_8 - GET_POST_TEMPLATE/login.html');
+    });
+
+    // Cadastrar usuário (GET via querystring)
+    app.get('/cadastrar', (req, res) => {
+      const { nome, email, senha, nascimento } = req.query;
+      console.log(nome, email, senha, nascimento);
+
+      res.render('resposta.ejs', {
+        mensagem: "Usuário cadastrado com sucesso!",
+        usuario: nome,
+        login: email
+      });
+    });
+
+    // Cadastrar usuário (POST com inserção no Mongo)
+    app.post('/resposta', (req, res) => {
+      const { nome, email, senha, nascimento } = req.body;
+      const data = { db_nome: nome, db_email: email, db_senha: senha, db_nascimento: nascimento };
+
+      usuarios.insertOne(data, (err) => {
+        if (err) {
+          res.render('resposta.ejs', {
+            mensagem: "Erro ao cadastrar usuário!",
+            usuario: nome,
+            login: email
+          });
+        } else {
+          res.render('resposta.ejs', {
+            mensagem: "Usuário cadastrado com sucesso!",
+            usuario: nome,
+            login: email
+          });
+        }
+      });
+    });
+
+    // Login
+    app.post('/login', function(requisicao, resposta){
+        let email = requisicao.body.email;
+        let senha = requisicao.body.senha;
+    
+        let data = {db_email: email, db_senha: senha};
+        usuarios.find(data).toArray(function(err, items){
+            if (items.length == 0) {
+                resposta.render('login.ejs', {mensagem: "Usuário/senha não encontrado!"})
+            } else if (err) {
+                resposta.render('login.ejs', {mensagem: "Erro ao logar usuário!"})
+            } else {
+                resposta.render('resposta_usuario.ejs', {mensagem: "Usuário logado com sucesso!", usuario: items[0].db_nome, login: items[0].db_email})
+            }
+        });
+    });
+    
+
+    // Atualizar usuário
+    app.post('/atualizar_usuario', (req, res) => {
+      const filtro = { db_email: req.body.email, db_senha: req.body.senha };
+      const atualizacao = { $set: { db_senha: req.body.novasenha } };
+
+      usuarios.updateOne(filtro, atualizacao, (err, result) => {
+        if (err) {
+          res.render('resposta_usuario.ejs', { mensagem: "Erro ao atualizar usuário!" });
+        } else if (result.modifiedCount === 0) {
+          res.render('resposta_usuario.ejs', { mensagem: "Usuário/senha não encontrado!" });
+        } else {
+          res.render('resposta_usuario.ejs', { mensagem: "Usuário atualizado com sucesso!" });
+        }
+      });
+    });
+
+    // Remover usuário
+    app.post('/remover_usuario', (req, res) => {
+      const filtro = { db_email: req.body.email, db_senha: req.body.senha };
+
+      usuarios.deleteOne(filtro, (err, result) => {
+        if (err) {
+          res.render('resposta_usuario.ejs', { mensagem: "Erro ao remover usuário!" });
+        } else if (result.deletedCount === 0) {
+          res.render('resposta_usuario.ejs', { mensagem: "Usuário/senha não encontrado!" });
+        } else {
+          res.render('resposta_usuario.ejs', { mensagem: "Usuário removido com sucesso!" });
+        }
+      });
+    });
+
+    // Exemplo de uso de EJS com for
+    app.get('/for_ejs', (req, res) => {
+      const num = parseInt(req.query.num);
+      res.render('exemplo_for.ejs', { tamanho: num });
+    });
+
+    // Iniciar servidor após a conexão com o banco
+    const server = http.createServer(app);
+    server.listen(80, () => {
+      console.log("Servidor rodando...".rainbow);
+    });
+  })
+  .catch(err => {
+    console.error("Erro ao conectar ao MongoDB:", err);
+  });
+
